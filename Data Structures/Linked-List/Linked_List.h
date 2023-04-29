02f9616806b30b500591673c52ab38e61f6576a8
amr8644 <amr8644@gmail.com>
2023-04-30 00:58:32 +0300

    Added Linked List

--- Data Structures/Linked-List/Linked_List.h
+++ Data Structures/Linked-List/Linked_List.h
@@ -1,21 +1,18 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-
-typedef struct Node
-{
-    int data;
-    struct Node *next;
+typedef struct Node {
+  int data;
+  struct Node *next;
 
 } Node;
 
-Node *node_create( int data );
-void node_find( Node *head, int data );
-void node_print( Node *head );
-void node_insert_at_tail( Node *head, int data );
-Node *node_insert_at_head( Node *head, int data );
-Node *node_delete_head( Node *head );
-void node_delete( Node **head , int data);
-void node_update( Node *head , int data , int new_data);
+Node *node_create(int value);
+void node_insert_at_index(int value, int index, Node **head);
+void node_insert_at_head(int value, Node **head);
+void node_insert_at_tail(int value, Node **head);
+int node_get(int index, Node **head);
+void node_remove(int index, Node **head);
+void node_print(Node *head);
 
 #endif
--- Data Structures/Linked-List/linked_list.c
+++ Data Structures/Linked-List/linked_list.c
@@ -1,13 +1,102 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 #include "Linked_List.h"
 
-int main( int argc, char *argv[] )
-{
-   return 0;
+int size = 0;
+int main(int argc, char *argv[]) {
+
+  Node *head = NULL;
+
+  node_insert_at_index(1, 0, &head);
+  node_insert_at_index(2, 1, &head);
+  node_insert_at_tail(3, &head);
+
+  node_remove(2, &head);
+  node_print(head);
+
+  return 0;
 }
 
+Node *node_create(int value) {
+  Node *node = (Node *)malloc(sizeof(Node));
 
+  node->data = value;
+  node->next = NULL;
+  return node;
+}
+
+void node_insert_at_index(int value, int index, Node **head) {
+  Node *node = node_create(value);
+  int i = 0;
+  Node *prev = *head;
+
+  size++;
+
+  if (*head == NULL || index == 0) {
+    node->next = *head;
+    *head = node;
+    return;
+  }
+
+  while (i < index - 1) {
+    prev = prev->next;
+    i++;
+  }
+
+  node->next = prev->next;
+  prev->next = node;
+}
+
+void node_insert_at_tail(int value, Node **head) {
+  node_insert_at_index(value, size, head);
+}
+void node_insert_at_head(int value, Node **head) {
+  node_insert_at_index(value, 0, head);
+}
+
+int node_get(int index, Node **head) {
+  int i = 0;
+  Node *tmp = *head;
+  if (*head == NULL) {
+    return 1;
+  }
+
+  while (i != index) {
+    tmp = tmp->next;
+    i++;
+  }
+  return tmp->data;
+}
+
+void node_remove(int index, Node **head) {
+  int i = 0;
+  Node *prev = *head;
+
+  if (*head == NULL || index > size)
+    return;
+
+  if (index == 0) {
+    *head = prev->next;
+    return;
+  }
+
+  while (i != index - 1) {
+    prev = prev->next;
+    i++;
+  }
+  prev->next = prev->next->next;
+  size--;
+}
+
+void node_print(Node *head) {
+  Node *tmp = head;
+
+  while (tmp != NULL) {
+    printf("%d->", tmp->data);
+    tmp = tmp->next;
+  }
+  printf("\n");
+}
